dcdf39ff9e38b2f95c63bd1af5c833ae210d7b24
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-04-10 16:05:12 -0300

    feat(descontar): função de descontar e aplicar juros a um valor

--- 04_funcoes/0407_desafio02.js
+++ 04_funcoes/0407_desafio02.js
@@ -10,3 +10,30 @@ Código Condição de pagamento:
 - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 
 */
+
+function descontar (valor, desconto){
+    return valor - (valor * desconto);
+}
+
+function aplicarJuros(valor, juros){
+    return valor + (valor * juros);
+}
+
+let valorProduto = 20.00;
+const seDebito = 0;
+const seDinheiro = 0;
+const seDuasVezes = 0;
+const seAcimaDeDuas = 1;
+
+if (seDebito === 1){
+    console.log("Valor do produto: R$" + valorProduto + "\nDesconto: 10%" + "\nTotal a pagar: R$" + descontar(valorProduto, 0.1));
+
+} else if (seDinheiro === 1){
+    console.log("Valor do produto: R$" + valorProduto + "\nDesconto: 15%" + "\nTotal a pagar: R$" + descontar(valorProduto, 0.15));
+
+} else if (seDuasVezes === 1) {
+    console.log("Valor do produto: R$" + valorProduto + "\nDesconto: Sem desconto" + "\nTotal a pagar: R$" + valorProduto);
+
+} else {
+    console.log("Valor do produto: R$" + valorProduto + "\nJuros: 10%" + "\nTotal a pagar: R$" + aplicarJuros(valorProduto, 0.1));
+}
